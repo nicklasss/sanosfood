57682492e08a5dc91c333d75a77017befd09fd49
humberto <hlduran@hotmail.com>
2015-04-01 12:45:17 -0500

    cambio

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -3,8 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
 
-	public function index()
-	{
+	public function index() {
 		$this->load->view('admin/home');
 	}
 }
