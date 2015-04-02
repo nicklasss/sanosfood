b210c15f291f11d5763e89302fbd1b8cff649c50
nicolas <nicoduran92@gmail.com>
2015-04-02 11:38:58 -0500

    debug error en login

--- application/views/admin/validarusuario.php
+++ application/views/admin/validarusuario.php
@@ -4,6 +4,6 @@ if($resultado){
 	print json_encode(array('res'=>'ok'));
     exit();
 }else{
-	print json_encode( array('res'=>'bad','msj'=>'Usuario o clave incorrectos') );
+	print json_encode( array('res'=>'bad','msj'=>'Usuario o clave incorrectos','debug'=>'recibo '.$this->input->post('usuario').' y '.$this->input->post('clave')) );
     exit();
 }
