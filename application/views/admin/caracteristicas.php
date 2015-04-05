4ae2652cb5eaa7b3b7b68f0861f51db4f86ec794
humberto <hlduran@hotmail.com>
2015-04-05 01:16:32 -0500

    arreglos en vista caracteristicas

--- application/views/admin/caracteristicas.php
+++ application/views/admin/caracteristicas.php
@@ -2,30 +2,54 @@
 <form class="form-horizontal" role="form">
 
 <?php foreach ($caracteristicas as $caracteristica) {
-		
 print '
-  <div class="form-group" style="border-bottom: 1px solid lightgrey">
+  <div class="form-group">
 	<label class="col-md-2 control-label">Nombre:</label>
     <div class="col-md-10 contenedor">
 	    <div class="editable row escondido">
 	    	<div class="col-md-8">
-		    	<input type="text" class="form-control entrada" value="'.$caracteristica->nombre;.'"/>
+		    	<input type="text" class="form-control entrada" value="'.$caracteristica->nombre.'"/>
     		</div>
 	    	<div class="col-md-4">
-        		<button type="button" class="btn btn-xs btn-success btn-guardar" data-atributo="nombre">Guardar</button>
+        		<button type="button" class="btn btn-xs btn-success btn-guardar" data-atributo="nombre" data-id="'.$caracteristica->id.'">Guardar</button>
         		<button type="button" class="btn btn-xs btn-danger btn-cancelar">Cancelar</button>
 	    	</div>	
     	</div>
 		<div class="mostrable row">
 	    	<div class="col-md-8">
-		    	<h4 class="mostrado">'.$caracteristica->nombre;.'?></h4>
+		    	<h4 class="mostrado">'.$caracteristica->nombre.'</h4>
     		</div>
 	    	<div class="col-md-4">
         		<button type="button" class="btn btn-xs btn-primary btn-editar"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Editar</button>
 	    	</div>	
     	</div>
     </div>
-  </div>';}?>
+  </div>';
+
+print '
+  <div class="form-group" style="border-bottom: 1px solid lightgrey">
+	<label class="col-md-2 control-label">Descripcion:</label>
+    <div class="col-md-10 contenedor">
+	    <div class="editable row escondido">
+	    	<div class="col-md-8">
+		    	<textarea type="text" class="form-control entrada">'.$caracteristica->descripcion.'</textarea>
+    		</div>
+	    	<div class="col-md-4">
+        		<button type="button" class="btn btn-xs btn-success btn-guardar" data-atributo="descripcion" data-id="'.$caracteristica->id.'">Guardar</button>
+        		<button type="button" class="btn btn-xs btn-danger btn-cancelar">Cancelar</button>
+	    	</div>	
+    	</div>
+		<div class="mostrable row">
+	    	<div class="col-md-8">
+		    	<h4 class="mostrado">'.$caracteristica->descripcion.'</h4>
+    		</div>
+	    	<div class="col-md-4">
+        		<button type="button" class="btn btn-xs btn-primary btn-editar"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Editar</button>
+	    	</div>	
+    	</div>
+    </div>
+  </div>';
+}?>
 
 </form>
 
@@ -41,7 +65,7 @@ $(document).ready(function() {
 	});
 	$('.btn-guardar').click( function(event){
 		valor = $(event.target).parent().parent().find('.entrada').val();
-		rta = guardar( $(event.target).attr("data-atributo") , valor ,function(rta){
+		rta = guardar( $(event.target).attr("data-id") , $(event.target).attr("data-atributo") , valor ,function(rta){
 			if(rta) {
 				$(event.target).parent().parent().parent().find('.mostrado').html(valor); 
 				$(event.target).parent().parent().parent().find('.mostrable').show();
@@ -51,13 +75,13 @@ $(document).ready(function() {
 	});
 });
 
-function guardar (atributo, valor, callback) {
+function guardar (id, atributo, valor, callback) {
   $.ajax({                                               // envio de los datos
-	    url: "<?php print base_url();?>producto/editar",
+	    url: "<?php print base_url();?>caracteristicas/editar",
 	    context: document.body,
 	    dataType: "json",
 	    type: "POST",
-	    data: {id : <?php print $this->uri->segment(3);?>, atributo  : atributo, valor : valor } })
+	    data: {id : id, atributo  : atributo, valor : valor } })
    .done(function(data) {                               // respuesta del servidor
     if(data.res=="ok") {callback(true)}
     else {alert(data.msj);callback(false)}
