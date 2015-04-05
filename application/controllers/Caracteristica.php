7b86905ebb8c126433985e53d803b36a2b498f27
nicolas <nicoduran92@gmail.com>
2015-04-05 01:27:31 -0500

    Logica editar caracteristica

---
+++ application/controllers/Caracteristica.php
@@ -0,0 +1,25 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Caracteristica extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+	public function editar(){
+		if(!$this->session->userdata('logeado_admin')){
+			$data['resultado'] = array('res'=>'bad','msj'=>'No autorizado.');
+			$this->load->view('caracteristica/editar', $data, FALSE);
+		}
+		$id = $this->input->post('id',TRUE);
+		$atributo = @$this->input->post('atributo',TRUE);
+		$valor = @$this->input->post('valor',TRUE);
+		$this->load->model('Caracteristica_model');
+		$data['resultado'] = $this->Caracteristica_model->editar($id,$atributo,$valor);
+		$this->load->view('caracteristica/editar', $data, FALSE);
+	}
+
+}
+
+/* End of file Caracteristica.php */
+/* Location: ./application/controllers/Caracteristica.php */
--- application/models/Caracteristicas_model.php
+++ application/models/Caracteristicas_model.php
@@ -11,7 +11,23 @@ class Caracteristicas_model extends CI_Model {
     function listar(){
     	$query = $this->db->get('caracteristicas');
     	return $query->result();
-    }		
+    }
+    function editar($id = NULL, $atributo = NULL, $valor = NULL){
+        if($id != NULL AND $atributo != NULL AND $valor != NULL){
+            $this->db->trans_start();
+            $object = array($atributo => $valor);
+            $this->db->where('id', $id);
+            $this->db->update('caracteristicas', $object);
+            $this->db->trans_complete();
+
+            if ($this->db->trans_status() === FALSE)
+            {
+                return array('res'=>'bad','msj'=>'Error en la edición.');
+            }else{
+                return array('res'=>'ok');
+            }
+        }
+    }
 
 }
 
