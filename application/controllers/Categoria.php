a801cf5bcdff9d455f8aebf81516e7c3ccc9b488
Nicolás Durán <nicoduran92@gmail.com>
2015-04-06 17:55:33 -0500

    Creación de inserción y eliminación de categorias

--- application/controllers/Categoria.php
+++ application/controllers/Categoria.php
@@ -18,6 +18,27 @@ class Categoria extends CI_Controller {
 		$data['resultado'] = $this->Categorias_model->editar($id,$atributo,$valor);
 		$this->load->view('categoria/editar', $data, FALSE);
 	}
+	public function crear(){
+		if(!$this->session->userdata('logeado_admin')) {
+			$data['resultado'] = array('res'=>'bad','msj'=>'No autorizado.');
+			$this->load->view('categoria/crear', $data, FALSE);
+		}
+		$nombre = @$this->input->post('nombre',TRUE);
+		$descripcion = @$this->input->post('descripcion',TRUE);
+		$this->load->model('Categorias_model');
+		$data['resultado'] = $this->Categorias_model->crear($nombre,$descripcion);
+		$this->load->view('categoria/crear', $data, FALSE);
+	}
+	public function eliminar(){
+		if(!$this->session->userdata('logeado_admin')) {
+			$data['resultado'] = array('res'=>'bad','msj'=>'No autorizado.');
+			$this->load->view('categoria/crear', $data, FALSE);
+		}
+		$id = @$this->input->post('id',TRUE);
+		$this->load->model('Categorias_model');
+		$data['resultado'] = $this->Categorias_model->eliminar($id);
+		$this->load->view('categoria/eliminar', $data, FALSE);
+	}
 
 }
 
--- application/models/Categorias_model.php
+++ application/models/Categorias_model.php
@@ -13,16 +13,39 @@ class Categorias_model extends CI_Model {
     	return $query->result();
     }	
 	function editar($id = NULL, $atributo = NULL, $valor = NULL){
-        if($id != NULL AND $atributo != NULL AND $valor != NULL){
-            $this->db->trans_start();
-            $object = array($atributo => $valor);
-            $this->db->where('id', $id);
-            $this->db->update('categorias', $object);
-            $this->db->trans_complete();
-            if ($this->db->trans_status() === FALSE) {
-                return array('res'=>'bad','msj'=>'Error en la edición.'); }
-            else{ return array('res'=>'ok'); }
-        }
+        if($id == NULL OR $atributo == NULL OR $valor == NULL){
+            return array('res'=>'bad','msj'=>'Error en la edición.'); }
+        $this->db->trans_start();
+        $object = array($atributo => $valor);
+        $this->db->where('id', $id);
+        $this->db->update('categorias', $object);
+        $this->db->trans_complete();
+        if ($this->db->trans_status() === FALSE) {
+            return array('res'=>'bad','msj'=>'Error en la edición.'); }
+        else{ return array('res'=>'ok'); }
+            
+    }
+    function crear($nombre = null, $descripcion = null){
+        if($nombre == NULL OR $descripcion == null){
+            return array('res'=>'bad','msj'=>'Error en la creación.'); }
+
+        if(strlen($nombre)<3){
+            return array('res'=>'bad','msj'=>'Ingresa un nombre adecuado'); }
+
+        $this->db->where('nombre', $nombre);
+        if($this->db->count_all_results('categorias')>0){
+            return array('res'=>'bad','msj'=>'Ya existe una categoría con este nombre'); }
+
+        $object = array('nombre' => $nombre, 'descripcion' => $descripcion);
+        $this->db->insert('categorias', $object);
+        return array('res'=>'ok');
+    }
+    function eliminar($id = null){
+        if($id == null){
+            return array('res'=>'bad','msj'=>'Error en la inserción.'); }
+        $this->db->where('id', $id);
+        $this->db->delete('categorias');
+        return array('res'=>'ok');
     }
 }
 
