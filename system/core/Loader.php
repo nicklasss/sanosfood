43083b57d6b5b1522672ce154fa64a9994dfbbd3
Nicolás Durán <nicoduran92@gmail.com>
2015-04-06 10:46:28 -0500

    Revert "Actualización de codeigniter"
    This reverts commit 34c9162e42bb668943d5bb072e9ff2f89a5a1c7c.

--- system/core/Loader.php
+++ system/core/Loader.php
@@ -1118,7 +1118,7 @@ class CI_Loader {
 				}
 				else
 				{
-					log_message('debug', $path.' exists, but does not declare '.$subclass);
+					log_message('debug', APPPATH.'libraries/'.$file_path.$subclass.'.php exists, but does not declare '.$subclass);
 				}
 			}
 		}
@@ -1307,7 +1307,10 @@ class CI_Loader {
 			}
 
 			// Load all other libraries
-			$this->library($autoload['libraries']);
+			foreach ($autoload['libraries'] as $item)
+			{
+				$this->library($item);
+			}
 		}
 
 		// Autoload models
