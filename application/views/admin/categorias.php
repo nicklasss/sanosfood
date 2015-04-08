2325c08900e671b948aee8dc421ed9a98f03bc53
humberto <hlduran@hotmail.com>
2015-04-07 19:42:46 -0500

    pruebas generales con borrado e inclusion

--- application/views/admin/categorias.php
+++ application/views/admin/categorias.php
@@ -1,91 +1,178 @@
 <h2>CATEGORIAS</h2>
 <form class="form-horizontal" role="form">
+<div class="row" style="border-bottom: 3px solid #999">
+	<div class="col-md-3"><h4><strong>Nombre</strong></h4></div>
+	<div class="col-md-6"><h4><strong>Descripción</strong></h4></div>
+</div>
+
 
 <?php foreach ($categorias as $categoria) {
 print '
-  <div class="form-group">
-	<label class="col-md-2 control-label">Nombre:</label>
-    <div class="col-md-10 contenedor">
-	    <div class="editable row escondido">
-	    	<div class="col-md-8">
-		    	<input type="text" class="form-control entrada" value="'.$categoria->nombre.'"/>
-    		</div>
-	    	<div class="col-md-4">
-        		<button type="button" class="btn btn-xs btn-success btn-guardar" data-atributo="nombre" data-id="'.$categoria->id.'">Guardar</button>
-        		<button type="button" class="btn btn-xs btn-danger btn-cancelar">Cancelar</button>
-	    	</div>	
-    	</div>
-		<div class="mostrable row">
-	    	<div class="col-md-8">
-		    	<h4 class="mostrado">'.$categoria->nombre.'</h4>
-    		</div>
-	    	<div class="col-md-4">
-        		<button type="button" class="btn btn-xs btn-primary btn-editar"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Editar</button>
-	    	</div>	
-    	</div>
+<div class="row registro">
+  <div class="editable escondido">
+ 	<div class="col-md-3 contenedor">
+    	<input type="text" class="form-control entnombre" value="'.$categoria->nombre.'"/>
+	</div>
+ 	<div class="col-md-7 contenedor">
+    	<textarea type="text" class="form-control entdescripcion">'.$categoria->descripcion.'</textarea>
+	</div>
+ 	<div class="col-md-2 contenedor">
+   		<button type="button" class="btn btn-xs btn-success btn-guardar" data-id="'.$categoria->id.'">Guardar</button>
+   		<button type="button" class="btn btn-xs btn-warning btn-cancelar">Cancelar</button>
+   	</div>	
+  </div>
+  <div class="mostrable">
+	<div class="col-md-3">
+    	<h4 class="salnombre" value="'.$categoria->nombre.'">'.$categoria->nombre.'</h4>
     </div>
-  </div>';
-
-print '
-  <div class="form-group" style="border-bottom: 1px solid lightgrey">
-	<label class="col-md-2 control-label">Descripcion:</label>
-    <div class="col-md-10 contenedor">
-	    <div class="editable row escondido">
-	    	<div class="col-md-8">
-		    	<textarea type="text" class="form-control entrada">'.$categoria->descripcion.'</textarea>
-    		</div>
-	    	<div class="col-md-4">
-        		<button type="button" class="btn btn-xs btn-success btn-guardar" data-atributo="descripcion" data-id="'.$categoria->id.'">Guardar</button>
-        		<button type="button" class="btn btn-xs btn-danger btn-cancelar">Cancelar</button>
-	    	</div>	
-    	</div>
-		<div class="mostrable row">
-	    	<div class="col-md-8">
-		    	<h4 class="mostrado">'.$categoria->descripcion.'</h4>
-    		</div>
-	    	<div class="col-md-4">
-        		<button type="button" class="btn btn-xs btn-primary btn-editar"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Editar</button>
-	    	</div>	
-    	</div>
-    </div>
-  </div>';
+ 	<div class="col-md-7">
+    	<h4 class="saldescripcion">'.$categoria->descripcion.'</h4>
+	</div>
+   	<div class="col-md-2">
+   		<button type="button" class="btn btn-xs btn-primary btn-editar"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Editar</button>
+   		<button type="button" class="btn btn-xs btn-danger btn-eliminar"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Eliminar</button>
+   	</div>	
+  </div>
+</div>';
 }?>
+
+
+
+<div class="row registro ultimo" style="border-top: 3px solid #999">
+  <div class="editable escondido">
+ 	<div class="col-md-3 contenedor">
+    	<input type="text" class="form-control entnombre" placeholder="Nombre"/>
+	</div>
+ 	<div class="col-md-7 contenedor">
+    	<textarea type="text" class="form-control entdescripcion" placeholder="Descripción"></textarea>
+	</div>
+ 	<div class="col-md-2 contenedor">
+   		<button type="button" class="btn btn-xs btn-success btn-agregar">Agregar</button>
+   		<button type="button" class="btn btn-xs btn-warning btn-cancelar">Cancelar</button>
+   	</div>	
+  </div>
+  <div class="mostrable">
+	<div class="col-md-10">
+	</div>
+   	<div class="col-md-2">
+   		<button type="button" class="btn btn-xs btn-success btn-nuevo"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Nuevo</button>
+   	</div>	
+  </div>
+</div>'
+
+
 
 </form>
 
 <script type="text/javascript">
 $(document).ready(function() { 
+
+	$(".registro").each(function(i){
+	   if(i%2==0){
+	      $(this).css("background-color", "#eee");
+	   }else{
+	      $(this).css("background-color", "#ddd");
+	   }
+	});
+
 	$('.btn-editar').click( function(event){
 		$(event.target).parent().parent().parent().find('.editable').show();
-		$(event.target).parent().parent().hide();
+		$(event.target).parent().parent().parent().find('.mostrable').hide();
 	});
+
 	$('.btn-cancelar').click( function(event){
+		$(event.target).parent().parent().parent().find('.mostrable').show();
+		$(event.target).parent().parent().parent().find('.editable').hide();
+	});
+
+	$('.btn-eliminar').click( function(event){
+		rta = confirm("presione ACEPTAR para confirmar borrado, o CANCEL para no borrar");
+		if (rta) {
+		   rta = eliminar( $(event.target).attr("data-id") ,function(rta){
+			   if(rta) {
+				  $(event.target).parent().parent().parent().find('.mostrable').hide(); 
+			   };
+		   });
+		}
+	});
+
+	$('.btn-nuevo').click( function(event){
+		$(event.target).parent().parent().parent().find('.editable').show();
+		$(event.target).parent().parent().parent().find('.mostrable').hide();
+
+
+
+
+
+
+
+
+	});
+
+	$('.btn-agregar').click( function(event){
+		$(".ultimo").append('<h2>nuevo registro</h2>')
+		$(event.target).parent().parent().parent().find('.mostrable').show();
+		$(event.target).parent().parent().parent().find('.editable').hide();
+	
+
+
+
+
+
+
+
+	});
+
+	$('.btn-guardar').click( function(event){
+		enombre = $(event.target).parent().parent().parent().find('.entnombre').val();
+		snombre = $(event.target).parent().parent().parent().find('.salnombre').html();
+		if(enombre !== snombre) {
+		   rta = guardar( $(event.target).attr("data-id") , "nombre" , enombre ,function(rta){
+			   if(rta) {
+				  $(event.target).parent().parent().parent().find('.salnombre').html(enombre); 
+			   };
+		   });
+		};
+		edescripcion = $(event.target).parent().parent().parent().find('.entdescripcion').val();
+		sdescripcion = $(event.target).parent().parent().parent().find('.saldescripcion').html();
+		if(edescripcion !== sdescripcion) {
+		   rta = guardar( $(event.target).attr("data-id") , "descripcion" , edescripcion ,function(rta){
+			   if(rta) {
+				  $(event.target).parent().parent().parent().find('.saldescripcion').html(edescripcion); 
+			   };
+		   });
+		};
 		$(event.target).parent().parent().parent().find('.mostrable').show();
 		$(event.target).parent().parent().hide();
 	});
-	$('.btn-guardar').click( function(event){
-		valor = $(event.target).parent().parent().find('.entrada').val();
-		rta = guardar( $(event.target).attr("data-id") , $(event.target).attr("data-atributo") , valor ,function(rta){
-			if(rta) {
-				$(event.target).parent().parent().parent().find('.mostrado').html(valor); 
-				$(event.target).parent().parent().parent().find('.mostrable').show();
-				$(event.target).parent().parent().hide();
-			}
-		});
-	});
+
 });
 
 function guardar (id, atributo, valor, callback) {
-  $.ajax({                                               // envio de los datos
-	    url: "<?php print base_url();?>categoria/editar",
-	    context: document.body,
-	    dataType: "json",
-	    type: "POST",
-	    data: {id : id, atributo  : atributo, valor : valor } })
-   .done(function(data) {                               // respuesta del servidor
-    if(data.res=="ok") {callback(true)}
-    else {alert(data.msj);callback(false)}
-    })
-   .error(function(){alert('No hay conexion');callback(false);})
+	$.ajax({                                               // envio de los datos
+	  url: "<?php print base_url();?>categoria/editar",
+	  context: document.body,
+	  dataType: "json",
+	  type: "POST",
+	  data: {id : id, atributo  : atributo, valor : valor } })
+	 .done(function(data) {                               // respuesta del servidor
+	  if(data.res=="ok") {callback(true)}
+	  else {alert(data.msj);callback(false)}
+	  })
+	 .error(function(){alert('No hay conexion');callback(false);})
+}
+
+function eliminar (id, callback) {
+	$.ajax({                                               // envio de los datos
+	  url: "<?php print base_url();?>categoria/eliminar",
+	  context: document.body,
+	  dataType: "json",
+	  type: "POST",
+	  data: {id : id } })
+	 .done(function(data) {                               // respuesta del servidor
+	  if(data.res=="ok") {callback(true)}
+	  else {alert(data.msj);callback(false)}
+	  })
+	 .error(function(){alert('No hay conexion');callback(false);})
 }
 </script>
