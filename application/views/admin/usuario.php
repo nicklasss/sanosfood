30908c65cf49dcac4367e41d3e47607624cb56d0
humberto <hlduran@hotmail.com>
2015-04-18 22:29:07 -0500

    arreglos

--- application/views/admin/usuario.php
+++ application/views/admin/usuario.php
@@ -1,10 +1,3 @@
-<div class="row registro">
-	<div class="col-md-2"></div>
-	<div class="col-md-8">
-		<h2>USUARIO</h2>
-	</div>
-</div>
-
 <form class="form-horizontal form-contenedor">
 
 <div class="row registro">
@@ -62,14 +55,15 @@
 				</tr>
 				</thead>
 				<tbody id="lista">
-<?php if (isset($pedidos)) {
-			foreach ($pedidos as $pedido) {
-			print	'<tr role="row">
-						<td>'.$pedidos->fecha.'</td>
-						<td>'.$pedidos->estado.'</td>
-					</tr>';
-			};
-		}
+<?php 	
+if (isset($usuario->pedidos)) {
+	foreach ($usuario->pedidos as $pedido) {
+		print	'<tr role="row">
+				<td>'.$pedidos->fecha.'</td>
+				<td>'.$pedidos->estado.'</td>
+				</tr>';
+	};
+}
 ?>
 				</tbody>
 			</table> 
@@ -78,106 +72,13 @@
 </div>
 
 
-
-
-
-
 <script type="text/javascript">
 $(document).ready(function() { 
 
 	$('.form-contenedor').on('click','.btn-limpiar',function(event){
 		limpiarpantalla();
 	});
-
-	$('.form-contenedor').on('click','.btn-guardar',function(event){
-		enombre = $('.form-contenedor').find('.entnombre').val();
-		edescripcion = $('.form-contenedor').find('.entdescripcion').val();
-		eingredientes = $('.form-contenedor').find('.entingredientes').val();
-		emarca = $('.form-contenedor').find('.entmarca').val();
-		eprecio = $('.form-contenedor').find('.entprecio').val();
-		epeso = $('.form-contenedor').find('.entpeso').val();
-		epesoneto = $('.form-contenedor').find('.entpesoneto').val();
-		elargo = $('.form-contenedor').find('.entlargo').val();
-		eancho = $('.form-contenedor').find('.entancho').val();
-		ealto = $('.form-contenedor').find('.entalto').val();
-		eexistencias = $('.form-contenedor').find('.entexistencias').val();
-		eestado = $('.form-contenedor').find('.entestado').val();
-
-		if(enombre == "" || edescripcion == "" || emarca == "" || eprecio == "" || epeso == "" || epesoneto == "" || eestado == "") {
-			alert("Los campos obligatorios deben estar diligenciados");
-			return false; }
-
-		if(validarnumeroentero(eprecio, "PRECIO")  == false) {return false;}
-		if(validarnumeroentero(epeso, "PESO")  == false) {return false;}
-		if(validarnumeroentero(epesoneto, "PESO NETO")  == false) {return false;}
-		if(elargo != "") {if(validarnumeroentero(elargo, "LARGO") == false) {return false;}}
-		if(eancho != "") {if(validarnumeroentero(eancho, "ANCHO") == false) {return false;}}
-		if(ealto != "")  {if(validarnumeroentero(ealto, "ALTO")  == false) {return false;}}
-		if(eexistencias != "")  {if(validarnumeroentero(eexistencias, "EXISTENCIAS")  == false) {return false;}}
-		
-		rta = crear( enombre, edescripcion, eingredientes, emarca, eprecio, epeso, epesoneto, elargo, eancho, ealto, eexistencias, eestado, function(rta){
-			if(rta) {
-				limpiarpantalla();
-		   };
-	   });
-	});
 });
 
-function crear (nombre, descripcion, ingredientes, marca, precio, peso, pesoneto, largo, ancho, alto, existencias, estado, callback) {
-	var datacaracteristicas = [];
-	$('input[name*="car"]:checked').each(function(){
-		if($(this).val()!=='chulo'){
-			var caracteristica = { "idcaracteristica" : $(this).attr('data-id') , "valor" : $(this).val() };
-			datacaracteristicas.push(caracteristica);
-		}
-	});
-	var datacategorias = [];
-	$('input[name*="cat"]:checked').each(function(){
-		var categoria = { "idcategoria" : $(this).attr('data-id') };
-		datacategorias.push(categoria);
-	});
-  $.ajax({                                               
-	    url: "<?php print base_url();?>producto/crear",
-	    context: document.body,
-	    dataType: "json",
-	    type: "POST",
-	    data: {nombre : nombre, descripcion : descripcion, ingredientes : ingredientes, precio : precio, marca : marca, pesoneto : pesoneto, 
-	    		  peso : peso, largo : largo, ancho : ancho, alto : alto, existencias : existencias, estado : estado,
-	           datacaracteristicas : JSON.stringify(datacaracteristicas), datacategorias : JSON.stringify(datacategorias) } })
-   .done(function(data) {                               // respuesta del servidor
-    if(data.res=="ok") {callback(true)}
-    else {alert(data.msj);callback(false)}
-    })
-   .error(function(){alert('No hay conexion');callback(false);})
-}
-
-function validarnumeroentero (valor, campo) {
-	for(var i = 0; i<valor.length;i++){
-		if(valor.charCodeAt(i)<48 || valor.charCodeAt(i)>57) {
-			alert(campo + " debe ser un número entero positivo");
-			return false;
-		}
-	}
-}
-
-function limpiarpantalla () {
-	$('.form-contenedor').find('.entnombre').val("");
-	$('.form-contenedor').find('.entdescripcion').val("");
-	$('.form-contenedor').find('.entingredientes').val("");
-	$('.form-contenedor').find('.entprecio').val("");
-	$('.form-contenedor').find('.entpeso').val("");
-	$('.form-contenedor').find('.entpesoneto').val("");
-	$('.form-contenedor').find('.entlargo').val("");
-	$('.form-contenedor').find('.entancho').val("");
-	$('.form-contenedor').find('.entalto').val("");
-	$('.form-contenedor').find('.entexistencias').val("");
-	$('.form-contenedor').find('.entestado').val("");
-	$('input[value*="chulo"]').each(function(){
-		$(this).prop("checked", true);
-	});
-	$('input[name*="cat"]').each(function(){
-		$(this).prop("checked", false);
-	});
-}
 
 </script>
