66c99ba53eb12888a4a43f5ff9f9c7409590bc36
humberto <hlduran@hotmail.com>
2015-04-22 11:04:39 -0500

    arrelos

--- application/views/admin/pedidosxestado.php
+++ application/views/admin/pedidosxestado.php
@@ -1,6 +1,6 @@
 <div class="row">
 	<div class="col-md-3"></div>
-	<div class="col-md-5"><h2>Pedidos en estado <?php print $this->uri->segment(3);?></h2></div>
+	<div class="col-md-5"><h2>Pedidos en estado <mark><?php print $this->uri->segment(3);?></mark></h2></div>
 </div>
 
 <div class="row">
@@ -31,13 +31,8 @@
 	</div>
 </div>
 <div class="row">
-<<<<<<< HEAD
 	<div class="col-md-12 center-text">
 		<?php print $this->pagination->create_links();?>
-=======
-	<div class="col-md-12" center-text>
-	<?php print $this->pagination->create_links();?>
->>>>>>> origin/master
 	</div>
 </div>
 
