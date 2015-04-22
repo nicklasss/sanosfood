5042f33d71a8b6ffd3e5852a1b2cb6915f25d03b
humberto <hlduran@hotmail.com>
2015-04-22 09:31:47 -0500

    arreglos

--- application/views/admin/pedidosxestado.php
+++ application/views/admin/pedidosxestado.php
@@ -3,8 +3,6 @@
 	<div class="col-md-5"><h2>Pedidos en estado <?php print $this->uri->segment(3);?></h2></div>
 </div>
 
-<?php print $this->uri->segment(3);?>
-
 <div class="row">
 	<div class="col-md-3"></div>
 	<div class="col-md-6 panel panel-default panel-usuarios">
