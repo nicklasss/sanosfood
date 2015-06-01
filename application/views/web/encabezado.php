ccba443ae9fbfba35864a0150eb8b74457172c39
Nicolás Durán <nicoduran92@gmail.com>
2015-06-01 15:25:41 -0500

    Creación de encabezado plantilla

---
+++ application/views/web/encabezado.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> 
+<html class="no-js ie6 oldie" lang="es_co" version="HTML+RDFa 1.1" xmlns:fb="http://www.facebook.com/2008/fbml"> <![endif]-->
+<!--[if IE 7]>    
+<html class="no-js ie7 oldie" lang="es_co" version="HTML+RDFa 1.1" xmlns:fb="http://www.facebook.com/2008/fbml"> <![endif]-->
+<!--[if IE 8]>    
+<html class="no-js ie8 oldie" lang="es_co" version="HTML+RDFa 1.1" xmlns:fb="http://www.facebook.com/2008/fbml"> <![endif]-->
+<!--[if IE 9]>    
+<html class="no-js ie9" lang="es_co" version="HTML+RDFa 1.1" xmlns:fb="http://www.facebook.com/2008/fbml"> <![endif]-->
+<!--[if gt IE 9]><!-->
+<html class="no-js" lang="es_co" version="HTML+RDFa 1.1" xmlns:fb="http://www.facebook.com/2008/fbml" xmlns:og="http://opengraphprotocol.org/schema/">
+<!--<![endif]-->
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title><?php print @$pregunta->titulo;?></title>
+		
+	<!-- Fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,300,700,800' rel='stylesheet' type='text/css'>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<meta name="description" content="<?php print @$pregunta->descripcion; ?>">
+	<meta name="keywords" content="<?php print @$pregunta->metakeywords;?>">
+
+	<meta name="robots" content="INDEX,FOLLOW" />
+
+	<link rel="canonical" href="http://www.1doc3.com/" />
+	<link rel="alternate" href="http://www.1doc3.com/" hreflang="es-co" />
+
+	<link rel="icon" href="https://s3.amazonaws.com/1doc3/img/favicon.png" type="image/x-icon" />
+	<link rel="shortcut icon" href="https://s3.amazonaws.com/1doc3/img/favicon.png">
+
+	<a href="https://plus.google.com/102789918912607192339" rel="publisher"></a>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<meta name="author" content="1doc3">
+	<meta name="msvalidate.01" content="4559AF8A1303FF6B1A5841E03D85A1C5" />
+
+	<meta property="og:url" content="<?php print base_url();?>" />
+	<meta property="og:type" content="website"/>
+	<meta property="og:image" content="https://s3.amazonaws.com/1doc3/img/logo-fb.png"/>
+	<meta property="og:title" content="1DOC3 | Haz preguntas anónimas de salud y recibe respuestas de médicos especialistas."/>
+	<meta property="og:site_name" content="1DOC3"/>
+	<meta property="og:description" content="Ingresa a 1doc3 y encuentra la mejor orientación médica en la web en español. Haz tus preguntas anónimas de temas médicos y recibe respuestas por médicos especialistas reales y totalmente validados. Información médica confiable a tu alcance." />
+	<meta property="fb:app_id" content="1440239009569539" />
+
+	<meta property="twitter:account_id" content="2364242874" />
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:title" content="1DOC3">
+	<meta name="twitter:description" content="Haz preguntas anónimas de salud y recibe respuestas de médicos especialistas.">
+	<meta name="twitter:url" content="http://www.1doc3.com/">
+	<meta name="twitter:site" content="@doctor1doc3">
+	<meta name="twitter:image" content="https://s3.amazonaws.com/1doc3/img/logo-fb.png">
+	<meta name="twitter:domain" content="www.1doc3.com">
+
+	<link rel="stylesheet" href="<?php print base_url();?>css2/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php print base_url();?>css/main.css">
+	<script type="text/javascript" src="<?php print base_url();?>js/modernizr.js"></script>
+
+</head>
