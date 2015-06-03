59535888b7f41052ddc104b52058007198defa96
Nicolás Durán <nicoduran92@gmail.com>
2015-06-03 10:49:57 -0500

    Home tuyo

--- application/views/web/home.php
+++ application/views/web/home.php
@@ -1,6 +1,106 @@
-<div class="row">
-    <div class="col-lg-12">
-        <h1 class="page-header">Pagina principal de SANOSFOODS</h1>
-    </div>
+
+
+<div class="row" style="background: #aaddff;">
+   <div class="row">
+      <div class="col-lg-4 col-lg-offset-1" style="border:0px;">
+         <div class="panel panel-default" style="background: #aaddff; line-height:180px; padding-top:130px; border:0px;">
+            <h3><strong>LOREM IPSUM</strong></h3>
+            <h1><strong>DOLOR SIT AMET</strong></h1>
+            <h4>LOREM IPSUM</h4>
+            <h5>CONSECTETUR ADIPISICING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE.</h5>
+            <div class="row">
+               <div class="col-lg-5">
+                  <button type="button" class="btn btn-md btn-success" id="btn-guardar">Ver Detales</button>
+               </div>
+            </div>           
+         </div>
+      </div>
+   </div>
+
+   <div class="row">
+      <div class="col-lg-1 col-lg-offset-9 text-right">
+         <div class="panel panel-default" style="background: #aaddff; line-height:60px; border:0px;">
+            <div class="row">
+               <div class="col-lg-6 text-right">
+                  <a href="#"><span class="badge"> <</span></a><br>
+               </div>
+               <div class="col-lg-6 text-left">
+                  <a href="#"><span class="badge"> ></span></a><br>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div> 
+
+<div class="row" style="background: #eeeeee;">
+   <div class="col-lg-4">
+      <div class="row">
+         <div class="col-lg-10 col-lg-offset-2">
+            <div class="panel panel-default" style="height:120px; margin-top:30px; border-radius:5px;"></div>
+         </div> 
+      </div>
+   </div> 
+   <div class="col-lg-4">
+      <div class="row">
+         <div class="col-lg-10 col-lg-offset-1">
+            <div class="panel panel-default" style="height:120px; margin-top:30px; border-radius:5px;"></div>
+         </div> 
+      </div>
+   </div> 
+   <div class="col-lg-4">
+      <div class="row">
+         <div class="col-lg-10">
+            <div class="panel panel-default" style="height:120px; margin-top:30px; border-radius:5px;"></div>
+         </div> 
+      </div>
+   </div> 
+</div> 
+
+<div class="row" style="background: #cccccc;">
+   <div class="col-lg-12 text-center">
+            <h4><strong>PRODUCTOS DESTACADOS</strong></h4>
+   </div>
 </div>
 
+<div class="row" style="background: #cccccc;">
+   <div class="col-lg-3">
+      <div class="row">
+         <div class="col-lg-9 col-lg-offset-3">
+            <div class="panel panel-default" style="height:240px; margin-top:30px; border-radius:5px;"></div>
+         </div> 
+      </div>
+   </div> 
+   <div class="col-lg-3">
+      <div class="row">
+         <div class="col-lg-9 col-lg-offset-2">
+            <div class="panel panel-default" style="height:240px; margin-top:30px; border-radius:5px;"></div>
+         </div> 
+      </div>
+   </div> 
+   <div class="col-lg-3">
+      <div class="row">
+         <div class="col-lg-9 col-lg-offset-1">
+            <div class="panel panel-default" style="height:240px; margin-top:30px; border-radius:5px;"></div>
+         </div> 
+      </div>
+   </div> 
+   <div class="col-lg-3">
+      <div class="row">
+         <div class="col-lg-9">
+            <div class="panel panel-default" style="height:240px; margin-top:30px; border-radius:5px;"></div>
+         </div> 
+      </div>
+   </div> 
+</div> 
+
+
+<div class="row" style="background: #dddddd;">
+   <div class="col-lg-8 col-lg-offset-2 text-center">
+      <div class="panel panel-default" style="height:300px; margin-top:30px; border-radius:5px; background:#dddddd;">
+         <h1><strong>LOREM IPSUM</strong></h1>
+         <h2>dolor sit awet, consectetur adipising elit,</h2>
+         <h2>sed do eiuswod tempor incididuut ut.</h2>
+      </div>
+   </div> 
+</div> 
