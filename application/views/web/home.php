8c662cd50b2e380be09f1ea897bbb473a84122da
humberto <hlduran@hotmail.com>
2015-06-03 18:07:51 -0500

    mejoras home

--- application/views/web/home.php
+++ application/views/web/home.php
@@ -22,10 +22,10 @@
          <div class="boton01">
             <div class="row">
                <div class="col-lg-6 text-right">
-                  <a href="#"><span class="badge"> <</span></a><br>
+                  <a href="#"><span class="badge"> <</span></a>
                </div>
                <div class="col-lg-6 text-left">
-                  <a href="#"><span class="badge"> ></span></a><br>
+                  <a href="#"><span class="badge"> ></span></a>
                </div>
             </div>
          </div>
@@ -276,7 +276,7 @@
             </div>
             <div class="row">
                <div class="col-lg-12 text-right">
-                  <button type="button" class="btn btn-xs btn-success" id="btn-guardar">Ver Detales</button>
+                  <button type="button" class="btn btn-xs btn-success">Ver Detales</button>
                </div>
             </div>
          </div>
@@ -306,37 +306,109 @@
             </div>
             <div class="row">
                <div class="col-lg-12 text-right">
-                  <button type="button" class="btn btn-xs btn-success" id="btn-guardar">Ver Detales</button>
+                  <button type="button" class="btn btn-xs btn-success">Ver Detales</button>
                </div>
             </div>
          </div>
       </div>
    </div> 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 </div> 
 
 
-<!---------------------------------------------------------------IMAGEN FINAL -->
-<div class="row" style="background: #dddddd;">
+<!---------------------------------------------------------------BANER 4 -->
+<div class="row baner04">
    <div class="col-lg-8 col-lg-offset-2 text-center">
-      <div class="panel panel-default" style="height:300px; margin-top:30px; border-radius:5px; background:#dddddd;">
+      <div class="textos01">
          <h1><strong>LOREM IPSUM</strong></h1>
          <h2>dolor sit awet, consectetur adipising elit,</h2>
          <h2>sed do eiuswod tempor incididuut ut.</h2>
       </div>
    </div> 
+</div> 
+
+
+<!---------------------------------------------------------------BANER FINAL -->
+<div class="row baner05">
+   <div class="col-lg-2 text-right">
+      <div class="textos02">
+         <h2><strong>sano's</strong></h2>
+      </div>
+   </div> 
+   <div class="col-lg-2 text-left">
+      <div class="textos03">
+         <h4><strong>Sano's</strong></h4>
+      </div>
+      <div class="textos04">
+         <h5><strong>Timeline</strong></h5>
+      </div>
+      <div class="textos04">
+         <h5><strong>Messages</strong></h5>
+      </div>
+      <div class="textos04">
+         <h5><strong>Statistic</strong></h5>
+      </div>
+      <div class="textos04">
+         <h5><strong>Settings</strong></h5>
+      </div>
+      <div class="textos04">
+         <h5><strong>Logout</strong></h5>
+      </div>
+   </div> 
+
+   <div class="col-lg-2 text-left">
+      <div class="textos03">
+         <h4><strong>Servicios</strong></h4>
+      </div>
+      <div class="textos04">
+         <h5><strong>Branding</strong></h5>
+      </div>
+      <div class="textos04">
+         <h5><strong>UX Design</strong></h5>
+      </div>
+      <div class="textos04">
+         <h5><strong>Prototype</strong></h5>
+      </div>
+      <div class="textos04">
+         <h5><strong>UI Design</strong></h5>
+      </div>
+      <div class="textos04">
+         <h5><strong>Develepment</strong></h5>
+      </div>
+   </div> 
+
+   <div class="col-lg-2 text-left">
+      <div class="textos03">
+         <h4><strong>Sobre nosotros</strong></h4>
+      </div>
+      <div class="textos04">
+         <h5><strong>Company</strong></h5>
+      </div>
+      <div class="textos04">
+         <h5><strong>Our Team</strong></h5>
+      </div>
+      <div class="textos04">
+         <h5><strong>Contact</strong></h5>
+      </div>
+      <div class="textos04">
+         <h5><strong>Terms and Conditions</strong></h5>
+      </div>
+      <div class="textos04">
+         <h5><strong><a href="#">Privacy Policy</a></strong></h5>
+      </div>
+   </div> 
+
+   <div class="col-lg-2 text-left">
+      <div class="textos03">
+         <h4><strong>Suscribirme</strong></h4>
+      </div>
+      <div class="textos04">
+         <input type="email" class="form-control" placeholder="email"/>
+         <button type="button" class="btn btn-xs btn-success"> ></button>
+      </div>
+      <div class="textos03">
+         <h5><strong>Siguenos</strong></h5>
+      </div>
+   </div> 
+
+
 </div> 
