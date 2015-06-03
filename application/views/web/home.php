62e4722a396c6ec3663e60a285d75f6fb95c07f4
humberto <hlduran@hotmail.com>
2015-06-03 16:22:03 -0500

    arreglo home

--- application/views/web/home.php
+++ application/views/web/home.php
@@ -1,14 +1,15 @@
 
 
-<div class="row" style="background: #aaddff;">
+<!---------------------------------------------------------------IMAGEN SUPERIOR -->
+<div class="row baner01">
    <div class="row">
-      <div class="col-lg-4 col-lg-offset-1" style="border:0px;">
-         <div class="panel panel-default" style="background: #aaddff; line-height:180px; padding-top:130px; border:0px;">
+      <div class="col-lg-4 col-lg-offset-1">
+         <div class="textos01">
             <h3><strong>LOREM IPSUM</strong></h3>
             <h1><strong>DOLOR SIT AMET</strong></h1>
             <h4>LOREM IPSUM</h4>
             <h5>CONSECTETUR ADIPISICING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE.</h5>
-            <div class="row">
+            <div class="row boton01">
                <div class="col-lg-5">
                   <button type="button" class="btn btn-md btn-success" id="btn-guardar">Ver Detales</button>
                </div>
@@ -16,10 +17,9 @@
          </div>
       </div>
    </div>
-
    <div class="row">
       <div class="col-lg-1 col-lg-offset-9 text-right">
-         <div class="panel panel-default" style="background: #aaddff; line-height:60px; border:0px;">
+         <div class="boton01">
             <div class="row">
                <div class="col-lg-6 text-right">
                   <a href="#"><span class="badge"> <</span></a><br>
@@ -33,68 +33,304 @@
    </div>
 </div> 
 
-<div class="row" style="background: #eeeeee;">
+<!---------------------------------------------------------------BANNER DE PLATOS -->
+<div class="row baner02">
    <div class="col-lg-4">
       <div class="row">
          <div class="col-lg-10 col-lg-offset-2">
-            <div class="panel panel-default" style="height:120px; margin-top:30px; border-radius:5px;"></div>
+            <div class="plato01"></div>
          </div> 
       </div>
    </div> 
    <div class="col-lg-4">
       <div class="row">
          <div class="col-lg-10 col-lg-offset-1">
-            <div class="panel panel-default" style="height:120px; margin-top:30px; border-radius:5px;"></div>
+            <div class="plato02"></div>
          </div> 
       </div>
    </div> 
    <div class="col-lg-4">
       <div class="row">
          <div class="col-lg-10">
-            <div class="panel panel-default" style="height:120px; margin-top:30px; border-radius:5px;"></div>
+            <div class="plato03"></div>
          </div> 
       </div>
    </div> 
 </div> 
 
+<!---------------------------------------------------------------PRODUCTOS -->
 <div class="row" style="background: #cccccc;">
    <div class="col-lg-12 text-center">
             <h4><strong>PRODUCTOS DESTACADOS</strong></h4>
    </div>
 </div>
 
-<div class="row" style="background: #cccccc;">
+<div class="row baner03" style="background: #cccccc;">
+
    <div class="col-lg-3">
       <div class="row">
          <div class="col-lg-9 col-lg-offset-3">
-            <div class="panel panel-default" style="height:240px; margin-top:30px; border-radius:5px;"></div>
-         </div> 
+            <div class="row">
+               <div class="col-lg-12">
+                  <div class="producto01"></div>
+               </div>
+            </div>     
+            <div class="row">
+               <div class="col-lg-10">
+                  <div class="row">
+                     <div class="col-lg-8 texto02"><h6><strong>LOREM IPSUM</strong></h6></div>
+                     <div class="col-lg-4 texto02"><h4>$35.000</h4></div>
+                  </div>
+               </div>   
+            </div>
+            <div class="row">
+               <div class="col-lg-12 texto02">
+                  <h6>Loren ipsum dolor sit amet, consecte tur adipiscing elit, sed do elusmod tempor.</h6>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-lg-12 text-right">
+                  <button type="button" class="btn btn-xs btn-success" id="btn-guardar">Ver Detales</button>
+               </div>
+            </div>
+         </div>
       </div>
    </div> 
+ 
    <div class="col-lg-3">
       <div class="row">
          <div class="col-lg-9 col-lg-offset-2">
-            <div class="panel panel-default" style="height:240px; margin-top:30px; border-radius:5px;"></div>
-         </div> 
+            <div class="row">
+               <div class="col-lg-12">
+                  <div class="producto01"></div>
+               </div>
+            </div>     
+            <div class="row">
+               <div class="col-lg-10">
+                  <div class="row">
+                     <div class="col-lg-8 texto02"><h6><strong>LOREM IPSUM</strong></h6></div>
+                     <div class="col-lg-4 texto02"><h4>$35.000</h4></div>
+                  </div>
+               </div>   
+            </div>
+            <div class="row">
+               <div class="col-lg-12 texto02">
+                  <h6>Loren ipsum dolor sit amet, consecte tur adipiscing elit, sed do elusmod tempor.</h6>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-lg-12 text-right">
+                  <button type="button" class="btn btn-xs btn-success" id="btn-guardar">Ver Detales</button>
+               </div>
+            </div>
+         </div>
       </div>
    </div> 
+
    <div class="col-lg-3">
       <div class="row">
          <div class="col-lg-9 col-lg-offset-1">
-            <div class="panel panel-default" style="height:240px; margin-top:30px; border-radius:5px;"></div>
-         </div> 
+            <div class="row">
+               <div class="col-lg-12">
+                  <div class="producto01"></div>
+               </div>
+            </div>     
+            <div class="row">
+               <div class="col-lg-10">
+                  <div class="row">
+                     <div class="col-lg-8 texto02"><h6><strong>LOREM IPSUM</strong></h6></div>
+                     <div class="col-lg-4 texto02"><h4>$35.000</h4></div>
+                  </div>
+               </div>   
+            </div>
+            <div class="row">
+               <div class="col-lg-12 texto02">
+                  <h6>Loren ipsum dolor sit amet, consecte tur adipiscing elit, sed do elusmod tempor.</h6>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-lg-12 text-right">
+                  <button type="button" class="btn btn-xs btn-success" id="btn-guardar">Ver Detales</button>
+               </div>
+            </div>
+         </div>
       </div>
    </div> 
+
    <div class="col-lg-3">
       <div class="row">
          <div class="col-lg-9">
-            <div class="panel panel-default" style="height:240px; margin-top:30px; border-radius:5px;"></div>
-         </div> 
+            <div class="row">
+               <div class="col-lg-12">
+                  <div class="producto01"></div>
+               </div>
+            </div>     
+            <div class="row">
+               <div class="col-lg-10">
+                  <div class="row">
+                     <div class="col-lg-8 texto02"><h6><strong>LOREM IPSUM</strong></h6></div>
+                     <div class="col-lg-4 texto02"><h4>$35.000</h4></div>
+                  </div>
+               </div>   
+            </div>
+            <div class="row">
+               <div class="col-lg-12 texto02">
+                  <h6>Loren ipsum dolor sit amet, consecte tur adipiscing elit, sed do elusmod tempor.</h6>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-lg-12 text-right">
+                  <button type="button" class="btn btn-xs btn-success" id="btn-guardar">Ver Detales</button>
+               </div>
+            </div>
+         </div>
       </div>
    </div> 
 </div> 
 
 
+
+
+<div class="row baner03" style="background: #cccccc;">
+
+   <div class="col-lg-3">
+      <div class="row">
+         <div class="col-lg-9 col-lg-offset-3">
+            <div class="row">
+               <div class="col-lg-12">
+                  <div class="producto01"></div>
+               </div>
+            </div>     
+            <div class="row">
+               <div class="col-lg-10">
+                  <div class="row">
+                     <div class="col-lg-8 texto02"><h6><strong>LOREM IPSUM</strong></h6></div>
+                     <div class="col-lg-4 texto02"><h4>$35.000</h4></div>
+                  </div>
+               </div>   
+            </div>
+            <div class="row">
+               <div class="col-lg-12 texto02">
+                  <h6>Loren ipsum dolor sit amet, consecte tur adipiscing elit, sed do elusmod tempor.</h6>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-lg-12 text-right">
+                  <button type="button" class="btn btn-xs btn-success" id="btn-guardar">Ver Detales</button>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div> 
+ 
+   <div class="col-lg-3">
+      <div class="row">
+         <div class="col-lg-9 col-lg-offset-2">
+            <div class="row">
+               <div class="col-lg-12">
+                  <div class="producto01"></div>
+               </div>
+            </div>     
+            <div class="row">
+               <div class="col-lg-10">
+                  <div class="row">
+                     <div class="col-lg-8 texto02"><h6><strong>LOREM IPSUM</strong></h6></div>
+                     <div class="col-lg-4 texto02"><h4>$35.000</h4></div>
+                  </div>
+               </div>   
+            </div>
+            <div class="row">
+               <div class="col-lg-12 texto02">
+                  <h6>Loren ipsum dolor sit amet, consecte tur adipiscing elit, sed do elusmod tempor.</h6>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-lg-12 text-right">
+                  <button type="button" class="btn btn-xs btn-success" id="btn-guardar">Ver Detales</button>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div> 
+
+   <div class="col-lg-3">
+      <div class="row">
+         <div class="col-lg-9 col-lg-offset-1">
+            <div class="row">
+               <div class="col-lg-12">
+                  <div class="producto01"></div>
+               </div>
+            </div>     
+            <div class="row">
+               <div class="col-lg-10">
+                  <div class="row">
+                     <div class="col-lg-8 texto02"><h6><strong>LOREM IPSUM</strong></h6></div>
+                     <div class="col-lg-4 texto02"><h4>$35.000</h4></div>
+                  </div>
+               </div>   
+            </div>
+            <div class="row">
+               <div class="col-lg-12 texto02">
+                  <h6>Loren ipsum dolor sit amet, consecte tur adipiscing elit, sed do elusmod tempor.</h6>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-lg-12 text-right">
+                  <button type="button" class="btn btn-xs btn-success" id="btn-guardar">Ver Detales</button>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div> 
+
+   <div class="col-lg-3">
+      <div class="row">
+         <div class="col-lg-9">
+            <div class="row">
+               <div class="col-lg-12">
+                  <div class="producto01"></div>
+               </div>
+            </div>     
+            <div class="row">
+               <div class="col-lg-10">
+                  <div class="row">
+                     <div class="col-lg-8 texto02"><h6><strong>LOREM IPSUM</strong></h6></div>
+                     <div class="col-lg-4 texto02"><h4>$35.000</h4></div>
+                  </div>
+               </div>   
+            </div>
+            <div class="row">
+               <div class="col-lg-12 texto02">
+                  <h6>Loren ipsum dolor sit amet, consecte tur adipiscing elit, sed do elusmod tempor.</h6>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-lg-12 text-right">
+                  <button type="button" class="btn btn-xs btn-success" id="btn-guardar">Ver Detales</button>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div> 
+
+
+<!---------------------------------------------------------------IMAGEN FINAL -->
 <div class="row" style="background: #dddddd;">
    <div class="col-lg-8 col-lg-offset-2 text-center">
       <div class="panel panel-default" style="height:300px; margin-top:30px; border-radius:5px; background:#dddddd;">
