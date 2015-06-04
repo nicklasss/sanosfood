bd7e720d55420c8ed2ec7001c90e054160ac5a71
humberto <hlduran@hotmail.com>
2015-06-04 09:14:16 -0500

    baneres al 100%

--- application/views/web/home.php
+++ application/views/web/home.php
@@ -1,39 +1,44 @@
 
+</div> <!-- /container -->
 
 <!---------------------------------------------------------------IMAGEN SUPERIOR -->
 <div class="row baner01">
-   <div class="row">
-      <div class="col-lg-4 col-lg-offset-1">
-         <div class="textos01">
-            <h3><strong>LOREM IPSUM</strong></h3>
-            <h1><strong>DOLOR SIT AMET</strong></h1>
-            <h4>LOREM IPSUM</h4>
-            <h5>CONSECTETUR ADIPISICING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE.</h5>
-            <div class="row boton01">
-               <div class="col-lg-5">
-                  <button type="button" class="btn btn-md btn-success" id="btn-guardar">Ver Detales</button>
-               </div>
-            </div>           
+   <div class="container">
+      <div class="row">
+         <div class="col-lg-4 col-lg-offset-1">
+            <div class="textos01">
+               <h3><strong>LOREM IPSUM</strong></h3>
+               <h1><strong>DOLOR SIT AMET</strong></h1>
+               <h4>LOREM IPSUM</h4>
+               <h5>CONSECTETUR ADIPISICING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE.</h5>
+               <div class="row boton01">
+                  <div class="col-lg-5">
+                     <button type="button" class="btn btn-md btn-success" id="btn-guardar">Ver Detales</button>
+                  </div>
+               </div>           
+            </div>
          </div>
       </div>
-   </div>
-   <div class="row">
-      <div class="col-lg-1 col-lg-offset-9 text-right">
-         <div class="boton01">
-            <div class="row">
-               <div class="col-lg-6 text-right">
-                  <a href="#"><span class="badge"> <</span></a>
-               </div>
-               <div class="col-lg-6 text-left">
-                  <a href="#"><span class="badge"> ></span></a>
+      <div class="row">
+         <div class="col-lg-1 col-lg-offset-9 text-right">
+            <div class="boton01">
+               <div class="row">
+                  <div class="col-lg-6 text-right">
+                     <a href="#"><span class="badge"> <</span></a>
+                  </div>
+                  <div class="col-lg-6 text-left">
+                     <a href="#"><span class="badge"> ></span></a>
+                  </div>
                </div>
             </div>
          </div>
       </div>
-   </div>
+   </div> <!-- /container -->
 </div> 
 
 <!---------------------------------------------------------------BANNER DE PLATOS -->
+<div class="container">
+
 <div class="row baner02">
    <div class="col-lg-4">
       <div class="row">
@@ -310,15 +315,21 @@
    </div> 
 </div> 
 
+</div> <!-- /container -->
+
 
 <!---------------------------------------------------------------BANER 4 -->
 <div class="row baner04">
-   <div class="col-lg-8 col-lg-offset-2 text-center">
-      <div class="textos01">
-         <h1><strong>LOREM IPSUM</strong></h1>
-         <h2>dolor sit awet, consectetur adipising elit,</h2>
-         <h2>sed do eiuswod tempor incididuut ut.</h2>
-      </div>
-   </div> 
+   <div class="container">
+      <div class="col-lg-8 col-lg-offset-2 text-center">
+         <div class="textos01">
+            <h1><strong>LOREM IPSUM</strong></h1>
+            <h2>dolor sit awet, consectetur adipising elit,</h2>
+            <h2>sed do eiuswod tempor incididuut ut.</h2>
+         </div>
+      </div> 
+   </div> <!-- /container -->
 </div> 
+
+<div class="container">
 
