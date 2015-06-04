be5c79fd745d0aa1490c0ab9959f5037d3fd5cfa
humberto <hlduran@hotmail.com>
2015-06-04 08:08:57 -0500

    home y piedepagina

--- application/views/web/home.php
+++ application/views/web/home.php
@@ -61,12 +61,11 @@
 <!---------------------------------------------------------------PRODUCTOS -->
 <div class="row" style="background: #cccccc;">
    <div class="col-lg-12 text-center">
-            <h4><strong>PRODUCTOS DESTACADOS</strong></h4>
+      <h4><strong>PRODUCTOS DESTACADOS</strong></h4>
    </div>
 </div>
 
 <div class="row baner03" style="background: #cccccc;">
-
    <div class="col-lg-3">
       <div class="row">
          <div class="col-lg-9 col-lg-offset-3">
@@ -189,10 +188,7 @@
 </div> 
 
 
-
-
 <div class="row baner03" style="background: #cccccc;">
-
    <div class="col-lg-3">
       <div class="row">
          <div class="col-lg-9 col-lg-offset-3">
@@ -326,89 +322,3 @@
    </div> 
 </div> 
 
-
-<!---------------------------------------------------------------BANER FINAL -->
-<div class="row baner05">
-   <div class="col-lg-2 text-right">
-      <div class="textos02">
-         <h2><strong>sano's</strong></h2>
-      </div>
-   </div> 
-   <div class="col-lg-2 text-left">
-      <div class="textos03">
-         <h4><strong>Sano's</strong></h4>
-      </div>
-      <div class="textos04">
-         <h5><strong>Timeline</strong></h5>
-      </div>
-      <div class="textos04">
-         <h5><strong>Messages</strong></h5>
-      </div>
-      <div class="textos04">
-         <h5><strong>Statistic</strong></h5>
-      </div>
-      <div class="textos04">
-         <h5><strong>Settings</strong></h5>
-      </div>
-      <div class="textos04">
-         <h5><strong>Logout</strong></h5>
-      </div>
-   </div> 
-
-   <div class="col-lg-2 text-left">
-      <div class="textos03">
-         <h4><strong>Servicios</strong></h4>
-      </div>
-      <div class="textos04">
-         <h5><strong>Branding</strong></h5>
-      </div>
-      <div class="textos04">
-         <h5><strong>UX Design</strong></h5>
-      </div>
-      <div class="textos04">
-         <h5><strong>Prototype</strong></h5>
-      </div>
-      <div class="textos04">
-         <h5><strong>UI Design</strong></h5>
-      </div>
-      <div class="textos04">
-         <h5><strong>Develepment</strong></h5>
-      </div>
-   </div> 
-
-   <div class="col-lg-2 text-left">
-      <div class="textos03">
-         <h4><strong>Sobre nosotros</strong></h4>
-      </div>
-      <div class="textos04">
-         <h5><strong>Company</strong></h5>
-      </div>
-      <div class="textos04">
-         <h5><strong>Our Team</strong></h5>
-      </div>
-      <div class="textos04">
-         <h5><strong>Contact</strong></h5>
-      </div>
-      <div class="textos04">
-         <h5><strong>Terms and Conditions</strong></h5>
-      </div>
-      <div class="textos04">
-         <h5><strong><a href="#">Privacy Policy</a></strong></h5>
-      </div>
-   </div> 
-
-   <div class="col-lg-2 text-left">
-      <div class="textos03">
-         <h4><strong>Suscribirme</strong></h4>
-      </div>
-      <div class="textos04">
-         <input type="email" class="form-control" placeholder="email"/>
-         <button type="button" class="btn btn-xs btn-success"> ></button>
-      </div>
-      <div class="textos03">
-         <h5><strong>Siguenos</strong></h5>
-      </div>
-   </div> 
-
-
-</div> 
