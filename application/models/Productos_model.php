6e2ccd1e5df1f8eae63a9e00821d8f002278d477
Nicolás Durán <nicoduran92@gmail.com>
2015-06-15 14:29:03 -0500

    cambios para compatibilidad del servidor

--- application/models/Productos_model.php
+++ application/models/Productos_model.php
@@ -1,5 +1,5 @@
 <?php
-if($_SERVER['REMOTE_ADDR']=='127.0.0.1'){
+if($_SERVER['REMOTE_ADDR']=='127.0.0.1' OR $_SERVER['REMOTE_ADDR'] == '::1'){
     require $_SERVER['DOCUMENT_ROOT'].'/sanosfood/aws/aws-autoloader.php';
 }else{
     require $_SERVER['DOCUMENT_ROOT'].'/aws/aws-autoloader.php';
